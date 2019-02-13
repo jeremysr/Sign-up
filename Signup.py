162fca3c8b741cfecd830826a1868c0faa8f2369
jeremysr <47540315+jeremysr@users.noreply.github.com>
2019-02-13 15:01:30 +1300

    Ticket list

---
+++ Signup.py
@@ -0,0 +1,6 @@
+Ticket = [
+    Ticket("Jeremy","hmjzone@gmail.com","20.02.2002",False),
+    Ticket("Jerry","jerry@gmail.com","14.06.2003",False),
+    Ticket("Moses","moses@gmail.com","01.04.2001",False),
+    Ticket("Dom","dom@gmail.com","15.03.2002",False)
+    ]
