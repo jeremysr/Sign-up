5d274eb48c088d717a136adfb860ee2d989d973e
jeremysr <47540315+jeremysr@users.noreply.github.com>
2019-02-15 12:38:27 +1300

    added some code
    added class and stuff

--- Signup.py
+++ Signup.py
@@ -1,10 +1,14 @@
-#ver1.1
-
-
-
-
-
-
+#ver1.2
+class Ticket: #class is an object that holds infomation
+    
+    _ids = count(0)
+    
+    def __init__(self, image, name, email, date_of_birth, check_in):
+        self.id = next(self.ids)
+        self.name = name
+        self.email = email
+        self.dob = date_of_birth
+        self.check_in = check_in
 
 
 #test data
@@ -14,3 +18,14 @@ Ticket = [
     Ticket("Moses","moses@gmail.com","01.04.2001",False),
     Ticket("Dom","dom@gmail.com","15.03.2002",False)
     ]
+
+
+
+#pages
+#index page
+@route("/")
+@view("index")
+def index():
+    pass
+
+run(host='0.0.0.0', port = 8080, reloader = True, debug = True)
