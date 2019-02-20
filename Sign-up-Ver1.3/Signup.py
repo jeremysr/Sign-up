672c017e3d4db23c964687fa843b71ca3cc04fa6
jeremysr <47540315+jeremysr@users.noreply.github.com>
2019-02-20 15:09:53 +1300

    Add files via upload

---
+++ Sign-up-Ver1.3/Signup.py
@@ -0,0 +1,35 @@
+from bottle import run, route, view, get, post, request
+from itertools import count
+
+#ver 1.3
+
+class Ticket: #class is an object that holds infomation
+
+    _ids = count(0)
+
+    def __init__(self,name, email, date_of_birth, check_in):
+        self.id = next(self._ids)
+        self.name = name
+        self.email = email
+        self.dob = date_of_birth
+        self.check_in = check_in
+
+
+#test data
+Ticket = [
+    Ticket("Jeremy","hmjzone@gmail.com","20.02.2002",False),
+    Ticket("Jerry","jerry@gmail.com","14.06.2003",False),
+    Ticket("Moses","moses@gmail.com","01.04.2001",False),
+    Ticket("Dom","dom@gmail.com","15.03.2002",False)
+]
+
+
+
+#pages
+#index page
+@route('/')
+@view('index')
+def index():
+    pass
+
+run(host='0.0.0.0', port = 8080, reloader = True, debug = True)
